c11c42ab218311d375e69c76dd7d774318c1dc83
Progress Ojemeh <54443717+Rex739@users.noreply.github.com>
2021-12-22 14:41:56 +0100

    Update team2.tsx

--- packages/client/src/components/landingpage/team/team2.tsx
+++ packages/client/src/components/landingpage/team/team2.tsx
@@ -16,6 +16,7 @@ const Team: NextPage = () => {
                 <a 
                 href={member.twitter}
                 target="_blank"
+                rel="noreferrer
                 >
                     <img 
                         src="images/socials/twitter2.svg" 
