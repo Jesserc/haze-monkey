0d6b8a4f6a49cc18041c346376e551afa367833b
alabobriggs <alabobriggs@gmail.com>
2021-12-29 22:00:01 +0100

    fix sizing

--- packages/client/src/components/landingpage/hero.tsx
+++ packages/client/src/components/landingpage/hero.tsx
@@ -7,8 +7,7 @@ const images: string[] = [
   'https://res.cloudinary.com/lab88/image/upload/v1640809390/website/d294ca30-c5c4-406e-9caf-c2ec76f3ca95_uabib1',
   'https://res.cloudinary.com/lab88/image/upload/v1640809390/website/c5f1d1b7-76dc-450d-a05d-88f9fa82db9a_ll0gu8',
   'https://res.cloudinary.com/lab88/image/upload/v1640809390/website/8a6dde98-1daa-4ac2-9390-7847db4ee78d_irspzi',
-  'https://res.cloudinary.com/lab88/image/upload/v1640809390/website/a41495aa-c654-4789-bc45-d06f38fffc74_vulaq9',
-  'https://res.cloudinary.com/lab88/image/upload/v1640809390/website/d294ca30-c5c4-406e-9caf-c2ec76f3ca95_uabib1'
+//   'https://res.cloudinary.com/lab88/image/upload/v1640809390/website/a41495aa-c654-4789-bc45-d06f38fffc74_vulaq9',
 ]
 
 const Hero: NextPage = () => {
@@ -17,16 +16,16 @@ const Hero: NextPage = () => {
       <div
         className={[
           styles.sectionContent,
-          'flex flex-col lg:flex-row items-center justify-center w-full relative'
+          'flex flex-col lg:flex-row items-center lg:items-start justify-center w-full relative'
         ].join(' ')}
       >
-        <div className="flex w-1/2 justify-center">
+        <div className="flex lg:w-1/2 mt-0 lg:mt-40 mb-10 lg:mb-0 justify-center">
           <div className="flex flex-col items-center lg:items-start text-center lg:text-left mx-auto">
             <img src="images/vectors/weed3.svg" alt="weed" className="mb-6" />
             <h1 className="mb-6">The OG Hazed Monkeys</h1>
             <p className="mb-6">The first residence of Hazron.</p>
             {/* <button className="app-btn">Mint NFT</button> */}
-            <button className="coming-soon flex items-center justify-center">
+            <button className="coming-soon bg-white flex items-center justify-center">
               Mint NFT{' '}
               <span className="flex items-center justify-center ml-2">
                 COMING SOON
@@ -34,7 +33,7 @@ const Hero: NextPage = () => {
             </button>
           </div>
         </div>
-        <div className="relative w-1/2 flex flex-wrap">
+        <div className="relative lg:w-1/2 flex flex-wrap">
           {images.map((img) => (
             <img key={img} src={img} className=" w-1/2" alt="NFTs" />
           ))}
