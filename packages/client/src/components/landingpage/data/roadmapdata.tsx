b7e69248150f0d796958884a6b02468241914978
alabobriggs <alabobriggs@gmail.com>
2022-01-06 11:29:38 +0100

    update roadmap

--- packages/client/src/components/landingpage/data/roadmapdata.tsx
+++ packages/client/src/components/landingpage/data/roadmapdata.tsx
@@ -1,57 +1,67 @@
 interface Roadmap {
-    id: number;
-    header: string;
-    content: string;
-    percentage: string;
+  id: number
+  header: string
+  content: string
+  percentage: string
 }
 
 export const roadmaps: Roadmap[] = [
-    {
-        id: 0,
-        header: "The Stoned Monkey Opportunity",
-        content: "We will Decentralise the film production industry through a gamified DAO voting system. Our Animated series will be the first project to test run this. Each art has a chance to appear in an episode. And 30% of the profit from animation will be used as a reward to the community.",
-        percentage: "images/vectors/roadmap1.svg"
-    },
-    {
-        id: 1,
-        header: "The Stoned Monkey Opportunity",
-        content:  "In line with United Nations SDG No. 14 (Life below water). We will work with Mr Beast’s Team sea Project to clean up to 100,000 pounds of waste from the sea by donating $100,000",
-        percentage: "images/vectors/roadmap2.svg"
-    },
-    {
-        id: 2,
-        header: "The Stoned Monkey Opportunity",
-        content: "We will plant 1 million trees over the next five years. One art mint equals 100 trees. We chose this because it cuts across the 17 United Nations SD goals and will promote more green NFT projects.",
-        percentage: "images/vectors/roadmap3.svg"
-    },
-    {
-        id: 3,
-        header: "The Stoned Monkey Opportunity",
-        content: "For every 5000 ETH secondary sale, 200ETH will be rewarded to our community.",
-        percentage: "images/vectors/roadmap4.svg"
-    },
-    {
-        id: 4,
-        header: "The Stoned Monkey Opportunity",
-        content: "Profit made from our secondary sales will be used to buy lands on Decentraland in order to build our virtual cinema. The episodes will be premiered on the virtual cinema, and only members of our community and partnering projects will have access.",
-        percentage: "images/vectors/roadmap5.svg"
-    },
-    {
-        id: 5,
-        header: "The Stoned Monkey Opportunity",
-        content:  "In preparation for our more expanded virtual cinema come 2023, we will give out 20 Oculus lenses to active members of our community. Those who do not win will be the first beta testers of the project.",
-        percentage: "images/vectors/roadmap5.svg"
-    },
-    {
-        id: 6,
-        header: "The Stoned Monkey Opportunity",
-        content: "Weed and psychedelic products are precious to the community’s mental health. We will explore possible ways to work with Marijuana research labs.",
-        percentage: "images/vectors/roadmap5.svg"
-    },
-    {
-        id: 7,
-        header: "The Stoned Monkey Opportunity",
-        content: "We will host a mental health session twice a month through Twitter Spaces, Clubhouse, Discord, or other lifestream platforms. These sessions will be open to the public but will feature only members of our community.",
-        percentage: "images/vectors/roadmap5.svg"
-    },
+  {
+    id: 0,
+    header: 'The Stoned Monkey Opportunity',
+    content: 'Launch of our genesis collection.',
+    percentage: 'images/vectors/roadmap1.svg'
+  },
+  {
+    id: 1,
+    header: 'The Stoned Monkey Opportunity',
+    content: 'Kick start the planting of trees.',
+    percentage: 'images/vectors/roadmap2.svg'
+  },
+  {
+    id: 2,
+    header: 'The Stoned Monkey Opportunity',
+    content: 'Release of the first 2 episodes of our animation series.',
+    percentage: 'images/vectors/roadmap3.svg'
+  },
+  {
+    id: 3,
+    header: 'The Stoned Monkey Opportunity',
+    content:
+      'We will host a mental health session twice a month through Twitter Spaces, Clubhouse, Discord, or other lifestream platforms. These sessions will be open to the public but will feature only members of our community',
+    percentage: 'images/vectors/roadmap4.svg'
+  },
+  {
+    id: 4,
+    header: 'The Stoned Monkey Opportunity',
+    content:
+      'Launch of our second collection(invaders) or collaborate with another project',
+    percentage: 'images/vectors/roadmap5.svg'
+  },
+  {
+    id: 5,
+    header: 'The Stoned Monkey Opportunity',
+    content:
+      'Collaborate with non fungible beats, to onboard metaverse musicians.',
+    percentage: 'images/vectors/roadmap5.svg'
+  },
+  {
+    id: 6,
+    header: 'The Stoned Monkey Opportunity',
+    content: 'Build out the next animation episodes',
+    percentage: 'images/vectors/roadmap5.svg'
+  },
+  {
+    id: 7,
+    header: 'The Stoned Monkey Opportunity',
+    content: 'Launch of our Gen 2 collection(alternate universe).',
+    percentage: 'images/vectors/roadmap5.svg'
+  },
+  {
+    id: 7,
+    header: 'The Stoned Monkey Opportunity',
+    content:
+      'Start building out our virtual cinema which will premiere our future episodes and other animation projects in the metaverse.',
+    percentage: 'images/vectors/roadmap5.svg'
+  }
 ]
