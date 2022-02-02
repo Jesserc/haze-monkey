e8ef0649163fe3c5def1c5a48197ac5dbef90462
alabobriggs <alabobriggs@gmail.com>
2022-02-02 21:36:45 +0100

    update image height

--- packages/client/utils/imageOptimizer.ts
+++ packages/client/utils/imageOptimizer.ts
@@ -1,3 +1,3 @@
 export const imageOptimizer = (image: string) => {
-  return `https://res.cloudinary.com/lab88/image/upload/w_auto,dpr_auto,q_auto,f_auto/v1643791432/website/${image}`
+  return `https://res.cloudinary.com/lab88/image/upload/w_350,dpr_auto,q_auto,f_auto/v1643791432/website/${image}`
 }
