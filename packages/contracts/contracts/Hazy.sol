b2b25eb3e96c08bb414f91fd8014c985cbdc0a94
alabobriggs <alabobriggs@gmail.com>
2022-02-08 13:11:38 +0100

    extract OZ contracts

--- packages/contracts/contracts/Hazy.sol
+++ packages/contracts/contracts/Hazy.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./Ownable.sol";
+import "./ReentrancyGuard.sol";
 import "./ERC721A.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "./Strings.sol";
 
 contract Hazy is Ownable, ERC721A, ReentrancyGuard {
     uint256 public immutable maxPerAddressDuringMint;
