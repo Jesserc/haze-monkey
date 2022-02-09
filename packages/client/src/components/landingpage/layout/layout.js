0959e6c6fa8a233cbd09d71ad1a00398e2020b12
alabobriggs <alabobriggs@gmail.com>
2022-02-09 11:00:17 +0100

    lint fix

--- packages/client/src/components/landingpage/layout/layout.js
+++ packages/client/src/components/landingpage/layout/layout.js
@@ -1,21 +1,17 @@
 // Page Layout
-import { NextPage } from "next";
-import NavBar from "../appNavbar/navbar";
-import Footer from "../footer";
+import { NextPage } from 'next'
+import NavBar from '../appNavbar/navbar'
+import Footer from '../footer'
 // Navbar and Footer Component
 
-
-const Layout = ({ handleClick, isClicked, children}) => {
-    return (
-        <>
-            <NavBar 
-                handleClick = { handleClick }
-                isClicked = { isClicked }
-            />
-                {children}
-            <Footer />
-        </>
-    );
+const Layout = ({ handleClick, isClicked, children }) => {
+  return (
+    <>
+      <NavBar handleClick={handleClick} isClicked={isClicked} />
+      {children}
+      <Footer />
+    </>
+  )
 }
 
-export default Layout;
+export default Layout
--- packages/client/src/pages/_app.js
+++ packages/client/src/pages/_app.js
@@ -1,16 +1,8 @@
 import '../styles/globals.css'
 import { useRouter } from 'next/router'
-// import { Provider } from "react-redux";
-// import { ToastProvider } from "react-toast-notifications";
-// import store from "../redux/store";
 import Layout from '../components/landingpage/layout/layout'
-import Loading from '../components/landingpage/loading/loading'
 import { useState, useEffect } from 'react'
 import { initGA, logPageView } from '../../utils/analytics'
-import { listRef } from '../components/landingpage/appNavbar/navList'
-
-import { gsap } from 'gsap/dist/gsap.js';
-import { TweenLite, Power3 } from 'gsap/dist/gsap.js';
 
 const MyApp = ({ Component, pageProps }) => {
   const [isClicked, setClicked] = useState(false)
@@ -19,19 +11,7 @@ const MyApp = ({ Component, pageProps }) => {
 
   const handleClick = () => {
     setClicked(!isClicked)
-     
-    // TweenLite.staggerFrom([listRef.children[0], listRef.children[1], listRef.children[2]], 0.2, {
-    //     opacity: 0, y: 10, ease: Power3.easeOut
-    // }, .4)
-            
   }
-
-  // useEffect(() => {
-  //   setLoading(true)
-  //   setTimeout(() => {
-  //     setLoading(false)
-  //   }, 3000);
-  // }, [])
 
   useEffect(() => {
     if (!window.GA_INITIALIZED) {
@@ -45,20 +25,14 @@ const MyApp = ({ Component, pageProps }) => {
   }, [router.pathname])
 
   return (
-  //  <Provider store={store}>
-  //     <ToastProvider autoDismissTimeout={3000}>
-       
-        <Layout handleClick={handleClick} isClicked={isClicked}>
-          <Component
-            {...pageProps}
-            handleclick={handleClick}
-            isClicked={isClicked}
-            loading = {loading}
-          />
-        </Layout>
-    //   </ToastProvider>
-    // </Provider>
-    
+    <Layout handleClick={handleClick} isClicked={isClicked}>
+      <Component
+        {...pageProps}
+        handleclick={handleClick}
+        isClicked={isClicked}
+        loading={loading}
+      />
+    </Layout>
   )
 }
 
