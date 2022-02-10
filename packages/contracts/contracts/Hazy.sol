e2cd071f1880acff033ae0ad3b58335c4c16ff7c
alabobriggs <alabobriggs@gmail.com>
2022-02-10 19:32:30 +0100

    update primint function

--- packages/contracts/contracts/Hazy.sol
+++ packages/contracts/contracts/Hazy.sol
@@ -40,11 +40,11 @@ contract Hazy is Ownable, ERC721A, ReentrancyGuard {
     }
 
     function presaleMint(uint256 quantity) external payable callerIsUser {
-        uint256 price = uint256(saleConfig.presalePrice);
+        uint256 price = uint256(saleConfig.presalePrice) * quantity;
         require(price != 0, "presaleList sale has not begun yet");
-        require(presaleList[msg.sender] < quantity, "Max amount already minted");
+        require(quantity <= presaleList[msg.sender], "Max amount already minted");
         require(presaleList[msg.sender] > 0, "not eligible for presaleList mint");
-        require(totalSupply() + 1 <= collectionSize, "reached max supply");
+        require(totalSupply() + quantity <= collectionSize, "reached max supply");
 
         presaleList[msg.sender] = presaleList[msg.sender] - quantity;
         _safeMint(msg.sender, quantity);
