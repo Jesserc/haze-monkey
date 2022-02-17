09c0036859f2d9011adcee7533298ef3a0703a8c
alabobriggs <alabobriggs@gmail.com>
2022-02-17 17:46:27 +0100

    add dynamic name

--- packages/client/src/pages/api/metadata/[id].js
+++ packages/client/src/pages/api/metadata/[id].js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   if (parseInt(req.query.id, 10) > 4200) throw new Error('You dey whine me')
 
   res.status(200).json({
-    name: 'Haze Monkey Society',
+    name: `HAZE #${req.query.id}`,
     description:
       '4200 The OG Haze Monkey Society The first residence of Hazron.',
     image:
